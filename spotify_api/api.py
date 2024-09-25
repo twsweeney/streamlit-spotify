7c36e7371bdd4a2d5f04364399527c19e83f3a94
Thomas Sweeney <twsweeney13@gmail.com>
2024-09-25 14:57:38 -0400

    ip typo lolzies

--- spotify_api/api.py
+++ spotify_api/api.py
@@ -15,7 +15,7 @@ class SpotifyAPI:
         self.CLIENT_SECRET = get_secret('spotify_client_secret')['spotify_client_secret']
 
         # public_ip = get_ec2_public_ip()
-        public_ip = 'http://18.216.76.240:8501/'
+        public_ip = '18.216.76.240'
         if public_ip:
             redirect_uri = f"http://{public_ip}:8501/callback"
             self.REDIRECT_URI = f"Redirect URI: {redirect_uri}"
