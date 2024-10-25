d760ca4d752a2f81d2ac5776c2bdc0a6b8774609
Thomas Sweeney <twsweeney13@gmail.com>
2024-10-25 13:52:37 -0400

    introduce game state

--- pages/heardle.py
+++ pages/heardle.py
@@ -14,7 +14,7 @@ def display_single_playlist_selector(df):
     
     selected_playlist = filtered_df[filtered_df['name'] == selected_playlist_name]
     selected_playlist_id = selected_playlist['playlist_id'].values[0]
-
+    st.session_state['round'] = 1
     return selected_playlist_id, selected_playlist_name
 
 
@@ -38,6 +38,54 @@ def get_audio_preview(song_id:str) -> str:
     return track_data
     # st.write(track_data)
 
+def get_song_data(current_user_id:str):
+    session = create_sqlalchemy_session()
+    playlist_df = get_playlist_df(session, current_user_id)
+
+    selected_playlist_id, selected_playlist_name = display_single_playlist_selector(playlist_df)
+
+    st.markdown(f'Selected playlist: {selected_playlist_name}')
+    song_id_list = get_songs(session=session, playlist_id=selected_playlist_id, current_user_id=current_user_id)
+    random_index = np.random.randint(0, len(song_id_list))
+
+    random_song_id = song_id_list[random_index]
+
+
+    song_data = get_audio_preview(random_song_id)
+    return song_data
+
+def play_audio():
+    round_durations_map = {
+        1:1,
+        2:3,
+        3:5,
+        4:10,
+        5:20,
+        6:30
+    }
+    snippet_duration = round_durations_map[st.session_state['round']]
+    st.write(f"Round {st.session_state['round']}: Listening for {snippet_duration} seconds")
+
+    if st.button('Play Audio'):
+        html_audio = f"""
+        <audio id="audio" src="{st.session_state['audio_url']}" autoplay></audio>
+        <script>
+        var audio = document.getElementById('audio');
+        audio.play();
+        setTimeout(() => {{ audio.pause(); }}, {snippet_duration * 1000}); // Stop after {snippet_duration} seconds
+        </script>
+        """
+
+        # Embed the HTML audio player in Streamlit without controls
+        st.components.v1.html(html_audio, height=0)
+
+def evaluate_answer(user_input):
+    clean_input = user_input.strip().lower()
+    answer = st.session_state['song_name'].strip().lower()
+
+    return clean_input == answer
+
+
 # pick a track
 def main():
     if 'user_id' not in st.session_state:
@@ -45,87 +93,58 @@ def main():
     else:
         current_user_id = st.session_state['user_id']
         current_user_display_name = st.session_state["display_name"]
+        MAX_ROUNDS = 6
 
-        st.markdown('# Compare your playlists!')
+
+        st.markdown('# Heardle')
         st.markdown(f'Currently logged in as: {current_user_display_name}')
 
-        session = create_sqlalchemy_session()
+        if 'game_state' not in st.session_state:
+            st.session_state['game_state'] = 'start'
 
+        if st.session_state['game_state'] == 'start':
+            song_data = get_song_data(current_user_id)
+            st.session_state['audio_url'] = song_data['preview_url']
+            st.session_state['song_name'] = song_data['name']
+            st.session_state['artists_name_list'] = [artist['name'] for artist in song_data['artists']]
+            st.session_state['game_state'] = 'guess'
         
-        playlist_df = get_playlist_df(session, current_user_id)
-
-        selected_playlist_id, selected_playlist_name = display_single_playlist_selector(playlist_df)
-
-        st.markdown(f'Selected playlist: {selected_playlist_name}')
-        song_id_list = get_songs(session=session, playlist_id=selected_playlist_id, current_user_id=current_user_id)
-        random_index = np.random.randint(0, len(song_id_list))
-
-        random_song_id = song_id_list[random_index]
 
 
-        song_data = get_audio_preview(random_song_id)
-
-        audio_url = song_data['preview_url']
-        song_name = song_data['name']
-        song_album = song_data['album']['name']
-        artists_name_list = [artist['name'] for artist in song_data['artists']]
-
-
-        # Initialize session state for round tracking
-        if 'round' not in st.session_state:
-            st.session_state['round'] = 1  
-            st.session_state['max_round'] = 6  
+        if st.session_state['game_state'] == 'guess':
+            if 'round' not in st.session_state:
+                st.session_state['round'] = 1
             st.session_state['correct_guess'] = False
 
-        round_durations_map = {
-            1:1,
-            2:3,
-            3:5,
-            4:10,
-            5:20,
-            6:None
-        }
-        snippet_duration = round_durations_map[st.session_state['round']]
-        st.write(f"Round {st.session_state.round}: Listening for {snippet_duration} seconds")
-
-        if st.button('Play Audio'):
-            html_audio = f"""
-            <audio id="audio" src="{audio_url}" autoplay></audio>
-            <script>
-            var audio = document.getElementById('audio');
-            audio.play();
-            setTimeout(() => {{ audio.pause(); }}, {snippet_duration * 1000}); // Stop after {snippet_duration} seconds
-            </script>
-            """
-
-            # Embed the HTML audio player in Streamlit without controls
-            st.components.v1.html(html_audio, height=0)
-
-
-
-        # st.audio(audio_url, end_time=snippet_duration, loop=True)
-        st.write(f'Correct answer: {song_name}')
-
+            if st.button('Play Audio Snippet'):
+                play_audio()
         
+            st.write(f'Correct answer for debugging: {st.session_state['song_name']}')
+            
+            user_guess = st.text_input("Guess the song name and artist (format: Song - Artist):")
+            if user_guess:
+                st.session_state['correct_answer'] = evaluate_answer(user_guess)
 
-        
-        st.session_state['guess_input'] = st.text_input("Guess the song name and artist (format: Song - Artist):")
+                # Set state to gameover if this is the last round, or they got it right
+                if st.session_state['round'] == 6 or st.session_state['correct_answer']:
+                    st.session_state['game_state'] = 'game_over'
 
-        # Button to move to the next round (guess feedback can be implemented here)
-        if st.button("Submit Answer"):
-            st.session_state['correct_guess'] = st.session_state['guess_input'].strip().lower() == song_name.strip().lower()
-        try:
-            if not st.session_state['correct_guess']:
-                # they guessed wrong
-                if st.session_state['round'] < st.session_state['max_round']:
-                    st.session_state['round'] += 1    
+                else:
+                    st.session_state['round'] += 1 
+
+            
+
+        if st.session_state['game_state'] == 'game_over':
+
+            if st.session_state['correct_answer']:
+                st.write('You win! congrats BUDDY')
             else:
-                st.markdown(f'You win! The correct song was: {song_name}')
-        except KeyError:
-            st.markdown('Play the audio clip and guess the song!')
+                st.write('You lost!!!!!!!!!!!!!!!!')
+            
+            del st.session_state['round']
 
-
-
+            if st.button('Click here to restart and play again'):
+                st.session_state['game_state'] = 'start'
 
 
 
